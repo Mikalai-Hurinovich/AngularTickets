cf038b435b5188d35f78f9bfc3829a74b0e1dab0
Mikalai-Hurinovich <gurinovich-ns@mail.ru>
2022-01-27 16:16:57 +0300

    features/jwt-token

--- src/app/shared/header/navbar/navbar.component.ts
+++ src/app/shared/header/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
@@ -7,13 +7,19 @@ import { Router } from '@angular/router';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {
+export class NavbarComponent implements DoCheck {
 
   constructor(
     public readonly authService: AuthService,
     private readonly router: Router,
+    private readonly cdr: ChangeDetectorRef,
     private readonly toastr: ToastrService) {
+  }
+  
+  ngDoCheck(): void {
+    this.cdr.markForCheck();
   }
 
   handleLogout() {
@@ -23,9 +29,11 @@ export class NavbarComponent {
         this.authService.isLoggedIn = false;
         this.router.navigate(['']);
         this.toastr.success('You was successfully logout');
+        this.cdr.markForCheck();
       },
       error: () => {
         this.toastr.error('Something went wrong...');
+        this.cdr.markForCheck();
       },
     });
   }
